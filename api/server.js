0887a145d491bcc965d79d583ec268267114c656
ericchurchill <m.ericchurchill@gmail.com>
2024-05-21 12:09:27 +0530

    Revert "tmp updated"
    This reverts commit e7550c849e6523435266f2a5e6a240fdc94dbeba.

--- api/server.js
+++ api/server.js
@@ -1,39 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const jsonServer = require('json-server');
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-// Change the path to a writable directory
-const dbFilePath = path.join('/tmp', 'db.json');
-const adapter = new FileSync(dbFilePath);
-const db = low(adapter);
-
-// Initialize LowDB database with default values if it doesn't exist
-db.defaults({}).write();
-
-// Create a JSON Server instance
-const server = jsonServer.create();
-const router = jsonServer.router(dbFilePath); // Use the same db.json file
-const middlewares = jsonServer.defaults();
-
-// Set middlewares
-server.use(middlewares);
-
-// Add custom rewrites before JSON Server router
+server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}));
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
 
-// Use the JSON Server router
-server.use(router);
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT}`);
-});
-
-// Export the Server API if needed
-module.exports = server;
+// Export the Server API
+module.exports = server
